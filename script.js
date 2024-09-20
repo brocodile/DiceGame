0c4229718bf175eabc4e10b25e5c0921c3ef8983
Dakshith L <dakzsh24@gmail.com>
2024-09-20 15:29:08 +0530

    Script Reload Button

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const playerOneScore = Math.floor((Math.random()*6) + 1); // 1-6
-const playerTwoScore = Math.floor((Math.random()*6) + 1); // 1-6
 const oneBox = document.querySelectorAll("#die1>*");
 const twoBox = document.querySelectorAll("#die2>*");
 const res = document.querySelector("#result");
@@ -13,6 +11,7 @@ const res = document.querySelector("#result");
 const coloring = [[], [4], [1,7], [1,4,7], [0,2,6,8], [0,2,4,6,8], [0,2,3,5,6,8]];
 function applyColors(box, arr){
     for(let i = 0; i < 9; i++){
+        box[i].style["visibility"] = "visible"; // reset to visible
         if (arr.includes(i)){
             box[i].style["background-color"] = "white";
         }else{
@@ -21,12 +20,19 @@ function applyColors(box, arr){
     }
 }
 
-if(playerOneScore === playerTwoScore){
-    res.textContent = "🚩Game Draw: No Winner🚩";
-}else if(playerOneScore > playerTwoScore){
-    res.textContent = "🚩 Player 1 Wins";
-}else{
-    res.textContent = "Player 2 Wins 🚩";
-}
-applyColors(oneBox, coloring[playerOneScore]);
-applyColors(twoBox, coloring[playerTwoScore]);
+
+const relbtn = document.querySelector("#relbtn");
+relbtn.addEventListener("click", () => {
+    relbtn.textContent = "Replay";
+    let playerOneScore = Math.floor((Math.random()*6) + 1); // 1-6
+    let playerTwoScore = Math.floor((Math.random()*6) + 1); // 1-6
+    if(playerOneScore === playerTwoScore){
+        res.textContent = "🚩Game Draw: No Winner🚩";
+    }else if(playerOneScore > playerTwoScore){
+        res.textContent = "🚩 Player 1 Wins";
+    }else{
+        res.textContent = "Player 2 Wins 🚩";
+    }
+    applyColors(oneBox, coloring[playerOneScore]);
+    applyColors(twoBox, coloring[playerTwoScore]);
+})
