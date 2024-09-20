8619a3f6a5407ca2abff0df6f4c250eb0035cfb8
Dakshith L <dakzsh24@gmail.com>
2024-09-20 13:51:33 +0530

    Add Die Logic

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ const playerOneScore = Math.floor((Math.random()*5) + 1); // 1-6
 const playerTwoScore = Math.floor((Math.random()*5) + 1); // 1-6
 const oneBox = document.querySelectorAll("#die1>*");
 const twoBox = document.querySelectorAll("#die2>*");
+const res = document.querySelector("#result");
 
-const one = [4];
-const two = [1,7];
-const three = [1,4,7];
-const four = [0,2,6,8];
-const five = [0,2,4,6,8];
-const six = [0,2,3,5,6,8];
-
+// const one = [4];
+// const two = [1,7];
+// const three = [1,4,7];
+// const four = [0,2,6,8];
+// const five = [0,2,4,6,8];
+// const six = [0,2,3,5,6,8];
+const coloring = [[], [4], [1,7], [1,4,7], [0,2,6,8], [0,2,4,6,8], [0,2,3,5,6,8]];
 function applyColors(box, arr){
     for(let i = 0; i < 9; i++){
         if (arr.includes(i)){
@@ -19,3 +20,13 @@ function applyColors(box, arr){
         }
     }
 }
+
+if(playerOneScore === playerTwoScore){
+    res.textContent = "🚩Game Draw: No Winner🚩";
+}else if(playerOneScore > playerTwoScore){
+    res.textContent = "🚩 Player 1 Wins";
+}else{
+    res.textContent = "Player 2 Wins 🚩";
+}
+applyColors(oneBox, coloring[playerOneScore]);
+applyColors(twoBox, coloring[playerTwoScore]);
